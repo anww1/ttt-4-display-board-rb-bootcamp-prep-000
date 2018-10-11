3cb381d88060267ef4b9c294a3d50728f51564e0
Alexandria Walker-Wade <alexandriawalkerwade@gmail.com>
2018-10-11 17:27:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,10 @@
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  
-end
+def display_board(board)
+puts " #{board[0]} |  #{board[1]} |  #{board[2]} "
+puts "-----------"
+puts " #{board[3]} |  #{board[4]} |  #{board[5]} "
+puts "-----------"
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-puts "prints a board with an X in the center position #{board[0]}"
+
+
 
 end
